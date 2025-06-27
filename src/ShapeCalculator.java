14bb96f0ecdee2a971c7390ca33617a983c42098
prabjotr <prabhjotbadhan1@gmail.com>
2025-06-27 16:18:01 +0530

    committing Seconds changes to vapasi

--- src/ShapeCalculator.java
+++ src/ShapeCalculator.java
@@ -4,73 +4,30 @@ import java.util.ArrayList;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 abstract class Shape {
 
-    abstract double calculateArea(double a,double b);
-    abstract double calculatePerimeter(double x,double y);
+    abstract double calculateArea();
+    abstract double calculatePerimeter();
 }
-
-class Circle extends Shape {
-
-
-    @Override
-    double calculateArea(double a, double b) {
-        return a*b;
-    }
-
-    @Override
-    double calculatePerimeter(double pi, double r ) {
-        double res;
-        res = 2 * pi * r;
-        return res;
-    }
-}
-
-class Reactangle extends Shape {
-    @Override
-    double calculateArea(double a, double b) {
-        return a*b;
-    }
-
-    @Override
-    double calculatePerimeter(double x, double y) {
-        double res;
-        res = (x+y) * 2;
-        return res;
-    }
-
-    double setDimensions(double lenWidth) {
-        return lenWidth * lenWidth;
-    }
-
-    double setDimensions(double length, double width) {
-
-        double result = length * width;
-        return result;
-    }
-}
-
-class Triangle extends Shape {
-    @Override
-    double calculateArea(double a, double b) {
-        return a*b;
-    }
-
-    @Override
-    double calculatePerimeter(double x, double y) {
-        double res;
-        res = (x*y)/2;
-        return res;
-    }
-}
-
-
 
 public class ShapeCalculator {
     public static void main(String[] args) {
         ArrayList<Shape> aObject = new ArrayList<>();
 
-        aObject.add(new Circle());
-        aObject.add(new Triangle());
-        aObject.add(new Reactangle());
+        //Run time polymorphism
+        aObject.add(new Circle(5,3.14));
+        aObject.add(new Triangle(5,6));
+        aObject.add(new Reactangle(3,2));
+
+        for(Shape sObject : aObject)
+        {
+            System.out.println("Area : "+sObject.calculateArea());
+            System.out.println("Perimeter : "+sObject.calculatePerimeter());
+        }
+
+        //Compile time polymorphism
+        Reactangle rObject = new Reactangle(3,2);
+        System.out.println("Circle "+rObject.setDimensions(5));
+        System.out.println("Reactangle : "+rObject.setDimensions(5.5,7));
+
 
         }
     }
